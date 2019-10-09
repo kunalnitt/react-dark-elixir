8a9feaf130d236d3ac1d01437a17a3d3875b4351
kunal15abc <kunal15abc@gmail.com>
2019-10-10 02:45:38 +0530

    Sidebar and Header first major release

--- src/HeaderBar/HeaderBar.js
+++ src/HeaderBar/HeaderBar.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import "./HeaderBar.css";
+import "@fortawesome/fontawesome-free/js/all.js"
+import "@fortawesome/fontawesome-free/css/all.css"
 
 const HeaderBar = (props) => {
-console.log("active",props.active )
+const toggleSidebarIcon = props.toggleSidebar !== undefined ? <div onClick={() => props.toggleSidebar()} className="rde-menu-icon"><i className="fas fa-bars fa-lg"></i></div> : null
 const menuItems = <div className="rde-menu">{props.items.map(function(item){
-return(<div className={item.name === props.active ? "rde-menu-item active" : "rde-menu-item"} onClick={() => {props.onChange(item),console.log("item",item.name)}}>{item.name}</div>)
+return(<div className={item.name === props.active ? "rde-menu-item active" : "rde-menu-item"} onClick={() => props.onChange(item)}>{item.name}</div>)
 })}</div>
   return (
     <div className="rde-header">
-     {menuItems}
+      {toggleSidebarIcon}
+      {menuItems}
     </div>
   );
 };
--- src/SideBar/SideBar.js
+++ src/SideBar/SideBar.js
@@ -7,14 +7,14 @@ const menuItems = <div className="rde-sidebar-menu">{props.items.map(function(it
   return(
       !item.list ? <div className={item.name === props.active ? "rde-sidebar-menu-item active" : "rde-sidebar-menu-item"} 
       onClick={() => props.onChange(item)}>{item.name}</div> : <div>
-      <div className="rde-sidebar-menu-dropdown">{item.name}<span className="caret"></span></div>
-      <div className="rde-sidebar-menu-dropdown-content">{item.list.map(function(listItem){
+      <div className="rde-sidebar-menu-dropdown">{item.name}<div className="rde-caret"><i class="fas fa-caret-down"></i></div></div>
+      <div className={item.list.map(function(listItem){return (listItem.name)}).includes(props.active) ? "rde-sidebar-menu-dropdown-content.active" : "rde-sidebar-menu-dropdown-content"}>{item.list.map(function(listItem){
         return(<div className={listItem.name === props.active ? "rde-sidebar-menu-dropdown-item active" : "rde-sidebar-menu-dropdown-item"} onClick={() => props.onChange(listItem)}>{listItem.name}</div>)
     })}</div></div>
   )
 })}</div>
   return (
-    <div className="rde-sidebar">
+    <div className={props.showSidebar===undefined || props.showSidebar ? "rde-sidebar" : "rde-sidebar active"}>
         {header}
         {menuItems}
     </div>
