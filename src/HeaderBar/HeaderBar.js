1b44becd25c67a0332a9c45bb120abf0d713f8d9
kunal15abc <kunal15abc@gmail.com>
2019-10-16 01:37:22 +0530

    Added menu icon to sidebar

--- src/HeaderBar/HeaderBar.js
+++ src/HeaderBar/HeaderBar.js
@@ -2,7 +2,8 @@ import React from 'react';
 import "./HeaderBar.css";
 
 const HeaderBar = (props) => {
-const toggleSidebarIcon = props.toggleSidebar !== undefined ? <div onClick={() => props.toggleSidebar()} className="rde-menu-icon"><i className="fas fa-bars fa-lg"></i></div> : null
+const toggleSidebarIcon = props.toggleSidebar !== undefined ? <div onClick={() => props.toggleSidebar()} className="rde-header-menu-icon"><i className="fas fa-bars fa-2x"></i></div> : null
+const logo = props.logo !== undefined ? <div className="rde-logo-container"><img src={props.logo} className="rde-logo"/></div> : null
 const userIcon = props.user !== undefined ? <div className="rde-user-icon"><i className="fas fa-user fa-lg"></i>  Welcome, {props.user}</div> : null
 const menuItems = props.items ? <div className="rde-menu">{props.items.map(function(item){
 return(<div className={item.name === props.active ? "rde-menu-item active" : "rde-menu-item"} onClick={() => props.onChange(item)}>{item.name}</div>)
@@ -10,6 +11,7 @@ return(<div className={item.name === props.active ? "rde-menu-item active" : "rd
   return (
     <div className="rde-header">
       {toggleSidebarIcon}
+      {logo}
       {menuItems}
       {userIcon}
     </div>
--- src/SearchBar/SearchBar.js
+++ src/SearchBar/SearchBar.js
@@ -2,10 +2,12 @@ import React from 'react';
 import "./SearchBar.css";
 
 const SearchBar = (props) => {
+    const toggleSidebarIcon = props.toggleSidebar !== undefined ? <div onClick={() => props.toggleSidebar()} className="rde-searchbar-menu-icon"><i className="fas fa-bars fa-lg"></i></div> : null
     return (
         <div className="rde-searchbar-container">
             <div className="rde-searchbar">
                 <div className="rde-searchbar-inner">
+                    {toggleSidebarIcon}
                     <div className="rde-searchbox-container">
                         <input type="text" className="rde-searchbox" placeholder="Search your item here..." onChange={props.handleInputChange}></input>
                     </div>
